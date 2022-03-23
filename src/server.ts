c461965ce483fa0d54c7ad505b1f964ed16e2ffa
Alan Atta <alan.atta@cubos.io>
2022-03-23 13:14:38 -0700

    fix: put back createConnection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import { createConnection } from 'typeorm';
 import { app } from './app';
 
 const PORT = process.env.PORT || 3333;
+
+createConnection();
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
