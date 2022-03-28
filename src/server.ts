086d039647e05f62d97fa1cf94fd5b853a59d4b6
Alan Atta <alan.atta@cubos.io>
2022-03-28 11:43:27 -0700

    test migration

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const env = process.env.NODE_ENV || 'local';
 
 getConnectionOptions()
     .then(async options => {
+        console.log({ options });
         const extra = env !== 'local' ? { ssl: { rejectUnauthorized: false } } : undefined;
         return createConnection({ ...options, extra, migrationsRun: true });
     })
