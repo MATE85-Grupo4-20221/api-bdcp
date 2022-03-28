962012ab9bb707948dac9c36aa4ebf740de55114
Alan Atta <alan.atta@cubos.io>
2022-03-28 01:08:25 -0700

    fix: lint error

--- src/controllers/ComponentController.ts
+++ src/controllers/ComponentController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { ComponentService } from '../services/ComponentService';
-import axios, { AxiosRequestConfig} from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import cheerio, { CheerioAPI } from 'cheerio';
 
 class ComponentController {
@@ -68,7 +68,7 @@ class ComponentController {
             headers: {
                 'Content-type': 'application/json'
             },
-        }
+        };
         const options2: AxiosRequestConfig = {
             method: 'get',
             url: '',
@@ -77,72 +77,71 @@ class ComponentController {
             headers: {
                 'Content-type': 'application/json'
             },
-        }
+        };
         const extractClassOB = ($: CheerioAPI) =>
             $('table').eq(2).find('tr')
                 .map((_: any, lesson: any) => {
-                    const $lesson = $(lesson); 
-                    
-                    return { 
-                        code: $lesson.find('td:nth-child(2)').text(), 
+                    const $lesson = $(lesson);
+                    return {
+                        code: $lesson.find('td:nth-child(2)').text(),
                         name: $lesson.find('td:nth-child(3)').text().trim(),
                         kind: $lesson.find('td:nth-child(4)').text().trim(),
-                        url: 'https://alunoweb.ufba.br' + $lesson.find('td:nth-child(3) a').attr('href') 
-                    }; 
-                }).toArray(); 
-                
+                        url: 'https://alunoweb.ufba.br' + $lesson.find('td:nth-child(3) a').attr('href')
+                    };
+                }).toArray();
+
         const extractFullClass = ($: CheerioAPI, content: { code: any; name: any; kind: any; }) =>
             $('table').eq(1)
                 .map((_: any, lesson: any) => {
-                    const $lesson = $(lesson); 
-                    
-                    return { 
-                        code: content.code, 
+                    const $lesson = $(lesson);
+
+                    return {
+                        code: content.code,
                         name: content.name,
                         kind:content.kind,
                         syllabus: $lesson.find('tr:nth-child(7) td:nth-child(1)').text(),
-                    }; 
+                    };
                 }).toArray();
-        
-            axios(options1)
+
+        axios(options1)
             .then(({ data }) => {
                 const decoder = new TextDecoder('ISO-8859-1');
-                let html = decoder.decode(data)
+                const html = decoder.decode(data);
                 const $ = cheerio.load(html);
                 const lessonsOB = extractClassOB($);
-                lessonsOB.splice(0,2)
-                let promises = [];
-                let lessonsArr: any[] = []
-                let promises2: any[] = [];
+                lessonsOB.splice(0,2);
+                const promises = [];
+                const lessonsArr: any[] = [];
+                const promises2: any[] = [];
 
                 for(const lesson of lessonsOB){
                     try{
-                        options2.url = lesson.url
+                        options2.url = lesson.url;
                         promises.push(
                             axios(options2).then(({ data }) => {
                                 const decoder = new TextDecoder('ISO-8859-1');
-                                let html = decoder.decode(data)
+                                const html = decoder.decode(data);
                                 const $ = cheerio.load(html); // Initialize cheerio
                                 const fullLessonsOB = extractFullClass($,lesson);
                                 lessonsArr.push(fullLessonsOB);
                             })
-                        )
+                        );
                     }
                     catch(err){
-                        console.log(err)
+                        console.log(err);
                     }
                 }
                 Promise.all(promises).then(() => {
                     for(const component of lessonsArr.flat()){
                         promises2.push(componentService.create(authenticatedUserId, component));
                     }
-                    Promise.all(promises2).then(() => {return response.status(201).json("All components created");});
+                    Promise.all(promises2).then(() => {return response.status(201).json('All components created');});
                 });
-        
-            })
+
+            });
 
         // const content = await componentService.create(authenticatedUserId, request.body);
-        
+
         // return response.status(201).json(content);
     }
 
