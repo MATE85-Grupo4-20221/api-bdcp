baa589fbbab9f9bfb8011f2aa89f44cefe29fc45
Iris Nogueira <ires.janine@hotmail.com>
2022-04-03 08:45:31 -0300

    Remove unnecessary contructor generating default values

--- src/entities/ComponentWorkload.ts
+++ src/entities/ComponentWorkload.ts
@@ -56,38 +56,6 @@ class ComponentWorkload {
     @OneToOne(() => Component, (component) => component.workload)
         component: Component;
 
-    constructor(){
-        if(this.studentTheory == null)
-            this.studentTheory = 0;
-        if(this.studentPractice == null)
-            this.studentPractice = 0;
-        if(this.studentInternship == null)
-            this.studentInternship = 0;
-        if(this.studentTheoryPractice == null)
-            this.studentTheoryPractice = 0;
-        if(this.studentPracticeInternship == null)
-            this.studentPracticeInternship = 0;
-        if(this.teacherTheory == null)
-            this.teacherTheory = 0;
-        if(this.teacherPractice == null)
-            this.teacherPractice = 0;
-        if(this.teacherInternship == null)
-            this.teacherInternship = 0;
-        if(this.teacherTheoryPractice == null)
-            this.teacherTheoryPractice = 0;
-        if(this.teacherPracticeInternship == null)
-            this.teacherPracticeInternship = 0;
-        if(this.moduleTheory == null)
-            this.moduleTheory = 0;
-        if(this.modulePractice == null)
-            this.modulePractice = 0;
-        if(this.moduleInternship == null)
-            this.moduleInternship = 0;
-        if(this.moduleTheoryPractice == null)
-            this.moduleTheoryPractice = 0;
-        if(this.modulePracticeInternship == null)
-            this.modulePracticeInternship = 0;
-    }
 }
 
 export { ComponentWorkload };
